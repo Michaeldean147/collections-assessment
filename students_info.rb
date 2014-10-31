c1c0ef71f4464963c2ed9a7c5a1314f5bd1951db
Michaeldean147 <michaeldean@MikeDean.local>
2014-10-31 10:22:32 -0700

    collections-assessment

---
+++ students_info.rb
@@ -0,0 +1,43 @@
+school_info = [
+
+                { First_Name: 'John',
+                  Last_Name: 'Foley',
+                  Email: 'john@gschool.it',
+                  Class: 'Beginning snark'},
+
+                { First_Name: 'Sylvester',
+                  Last_Name: 'Kelsey',
+                  Email: 'sellie@gmail.com',
+                  Class: 'Ruby Immersive'},
+
+                { First_Name: 'Timothy',
+                  Last_Name: 'Rama',
+                  Email: 'tim.rama@gmail.com',
+                  Class: 'Ruby Immersive' },
+
+                { First_Name: 'Kane',
+                  Last_Name: 'Baccigalupi',
+                  Email:  'kane@gschoo.it',
+                  Class: 'C for dummies' },
+
+                { First_Name: 'Nikita',
+                  Last_Name: 'Theodosious',
+                  Email: 'nikita.theo@gmail.com',
+                  Class: 'Ruby Immersive' },
+
+                { First_Name: 'Roddy',
+                  Last_Name: 'Eldred',
+                  Email: 'roddy.el@gmail.com',
+                  Class: 'Ruby Immersive'},
+
+                { First_Name: 'Martha',
+                  Last_Name: 'Berner',
+                  Email: 'martha@gschool.com',
+                  Class: 'Time travel for beginners'},
+
+                { First_Name: 'Kofi',
+                  Last_Name: 'Thomas',
+                  Email: 'k.thomas@hotmail.com',
+                  Class: 'Ruby Immersive'}
+
+                ]
